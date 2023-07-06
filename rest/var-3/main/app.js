329de9f13ce892d195ee4863fb115800ddf6779c
AT <hypothetical.andrei@gmail.com>
2023-07-06 16:24:36 +0300

    .

---
+++ rest/var-3/main/app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('sqlite:memory')
+
+class Device extends Sequelize.Model { };
+
+Device.init({
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING,
+    },
+    price: {
+        type: Sequelize.FLOAT,
+    }
+}, {
+    sequelize,
+    modelName: 'devices',
+    timestamps: false
+});
+
+const app = express();
+app.use(express.json());
+
+app.get('/create', async (req, res) => {
+    await sequelize.sync({force: true});
+    for(let i = 0; i < 10; i++) {
+        await Device.create({name: `Device-${i}`, price: `${Math.random() * 100 + i + 10}`});
+    }
+    res.status(201).json({message: 'devices created'});
+})
+
+app.get('/device', async (req, res) => {
+    const devices = await Device.findAll();
+    res.status(200).send(devices);
+})
+
+app.post('/device', async (req, res) => {
+    
+})
+
+app.delete('/device/:id', async (req, res) => {
+   
+})
+
+module.exports = app;
